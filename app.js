2251745cd3530e7ff5bbb76b37bb8c6bbccb111b
Peter Busatti <peterbusatti@gmail.com>
2022-01-09 10:59:44 -0500

    create table outline and card mockup

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
+let myLibrary = [];
+
 function Book(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        let complete;
-        if (read) {
-            complete = "have read";
-        }
-        else if (!read) {
-            complete = "not read yet"
-        }
-
-        return name + " by " + author + ", " + pages + ", " + complete;
-    }
 }
 const mistborn = new Book("Mistborn", "Brandon Sanderson", 544, true);
 
-console.log(mistborn.info());
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+addBookToLibrary(mistborn);
