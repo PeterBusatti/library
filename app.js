61f6534ae50f4c76f60361444806fb69d99d66ec
Peter Busatti <peterbusatti@gmail.com>
2022-01-11 13:31:21 -0500

    add delete buton and organize the addCards function
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- app.js
+++ app.js
@@ -10,48 +10,60 @@ function Book(name, author, pages, genre, read) {
     this.read = read;
 }
  
-const mistborn = new Book("Mistborn", "Brandon Sanderson", 544, "Fantasy", false);
+const mistborn = new Book("Mistborn", "Brandon Sanderson", 544, "Fantasy", true);
 
 function addBookToLibrary(book) {
+    
     myLibrary.push(book);
 }
 
 addBookToLibrary(mistborn);
 
-myLibrary.forEach(item => addCard(item));
+function addCards() {
+    myLibrary.forEach(item => {
+        const card = document.createElement("div");
+        card.classList.add("card"); 
 
-function addCard(item) {
-    let card = document.createElement("div");
-    card.classList.add("card"); 
-        
-    let togBtn = document.createElement("button");
-    togBtn.textContent = "Complete?";
-    togBtn.classList.add("switch");
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("delete", "btn");
+        deleteBtn.setAttribute("id", "delete");
 
-    let title = document.createElement("p");
-    let author = document.createElement("p");
-    let pages = document.createElement("p");
-    let genre = document.createElement("p");
+        const title = document.createElement("p");
+        title.textContent = item.name;
+        title.style.fontWeight = "bold";
+        title.style.fontSize = "22px";
 
-    title.textContent = item.name;
-    author.textContent = `By ${item.author}`;
-    pages.textContent = `Pages: ${item.pages}`;
-    genre.textContent = `Genre: ${item.genre}`;
+        const author = document.createElement("p");
+        author.textContent = `By ${item.author}`;
 
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(genre);
-    card.appendChild(togBtn);
+        const pages = document.createElement("p");
+        pages.textContent = `Pages: ${item.pages}`;
 
-    tableContainer.appendChild(card);
+        const genre = document.createElement("p");
+        genre.textContent = `Genre: ${item.genre}`;        
 
-    if (item.read) {
-        card.classList.toggle("read");
-    }
+        const togBtn = document.createElement("button");
+        togBtn.textContent = "Complete?";
+        togBtn.classList.add("btn");
+        togBtn.setAttribute("id", "complete");
+
+        card.appendChild(deleteBtn);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(genre);
+        card.appendChild(togBtn);
+
+        tableContainer.appendChild(card);
+
+        if (item.read) {
+            card.classList.toggle("read");
+        }
+    });
 }
 
-
+addCards();
 
 
 
